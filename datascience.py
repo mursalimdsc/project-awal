6a72bda1684cf87a02367f3f13f43d1e2605c20c
mursalimdsc <117145822+mursalimdsc@users.noreply.github.com>
2022-11-11 10:32:55 +0700

    Melakukan perubahan file datascience numpy
    Menambahkan beberapa variabel tentang eksplorasi tentang numpy

--- datascience.py
+++ datascience.py
@@ -1,2 +1,10 @@
 # Data Science
 print("Hallo Data Science")
+
+# Manipulasi numpy pada pyhton
+import numpy as np
+s = np.zeros(10)
+sx = np.ones(10)
+ab = np.arange(10,51)
+rand_normal = np.random.randn(5,5)
+print(rand_normal)
