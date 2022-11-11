35e389df3c9e70a5d16d79ab817050053d4f3b8f
mursalimdsc <117145822+mursalimdsc@users.noreply.github.com>
2022-11-11 19:41:44 +0700

    Menambah beberapa fitur baru pada file datascience
    Ada beberapa fitur yang ditambahkan seperti mengimport matplotlib dan numpy untuk menunjang fitur baru yakni pemodelan ekponensial kasus covid 19 di Indonesaia

--- datascience.py
+++ datascience.py
@@ -1,5 +1,23 @@
-# Data Science
-print("Hallo Data Science")
+#  import library
+import matplotlib.pyplot as plt
+import numpy as np
+
+#inisialisasi matplotlib
+%matplotlib inline
+
+x = np.linspace(0,5,11)
+y = x ** 2
+
+#fungsional ekponensial
+plt.plot(x,y,'r-')
+plt.xlabel('Bulan ke-')
+plt.ylabel('Jumlah Kasus Covid 19 di Indonesia')
+plt.title("Contoh Grafik Ekponensial kasus Covid 19 di Indonesia")
+
+plt.subplot(1,2,1)
+plt.plot(x,y,'r')
+plt.subplot(1,2,2)
+plt.plot(y,x,'b')
 
 # Manipulasi numpy pada pyhton
 import numpy as np
